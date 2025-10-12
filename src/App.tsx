38d98a40b36aa68b89d2b6a1b016611b5f1e3b4d
igadu <darylgzmn@gmail.com>
2025-10-11 21:17:43 -1000

    style: add header chore: setup absolute import paths in tsconfig and vite.config for cleaner imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import './App.css'
-import Home from "./pages/Home.tsx"
+import Home from "@/pages/Home.tsx"
+
+
 function App() {
   return (
     <>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,13 @@
 import "./Home.css";
+//import Header from "../components/Header/Header.tsx";
+import Header from "@/components/Header/Header.tsx"
 export default function Home(){
   return (
-    <div className="main-background"></div>
+    <>
+      <div className="main-background">
+        <Header /> 
+      </div>
+      
+    </>
   );
 }
