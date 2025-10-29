a8f0c46a7d43e3f5f737b2df8b6081620578d15e
Daryl Guzman <darylgzmn@gmail.com>
2025-10-28 21:44:30 -1000

    feat: Add file explorer logic

--- src/components/Layout/Cert/Cert.tsx
+++ src/components/Layout/Cert/Cert.tsx
@@ -1,14 +1,57 @@
 import "./Cert.css";
 import Windows from "@/components/UI/Windows.tsx";
 import NavPane from "@/components/UI/FileExplorer/NavPane.tsx";
-export default function Cert(){
+
+//Import Images 
+import APlus from  "@/assets/cert/A+-svg.svg";
+import NetPlus from "@/assets/cert/Network+-svg.svg";
+import SecPlus from "@/assets/cert/Security+-svg.svg";
+import AzureFun from  "@/assets/cert/Azure-Fundamentals.png";
+import {useState} from "react";
+
+interface CertProps{}
+
+interface ImageURLS {
+  key:number;
+  src:string;
+  label:string;
+}
+const certImageUrls = [ 
+  {key:1, src:APlus,    label:"Comptia A+"},
+  {key:2, src:NetPlus,  label:"Comptia Net+"},
+  {key:3, src:SecPlus,  label:"Comptia Sec+"},
+  {key:4, src:AzureFun, label:"Azure Fundamentals"},
+//{key:5, src:"sdfsdfsdf", label:"Azure Fundamentals"},
+];
+
+const Cert: React.FC<CertProps> = () => {
+  const [activeFolder, setActiveFolder] = useState<string>('Certificate');
+  const [imageURLS,setImageURLS] = useState<ImageURLS[] | null>(certImageUrls);
+
+
+  const handleDataFromNavPane = (data:string) => {
+    setActiveFolder(data);
+    if(data == 'Certificate') setImageURLS(certImageUrls);
+    else setImageURLS(null);
+    //console.log(activeFolder);
+  }
   return (
     <div className="cert-container">
-      <Windows  title='Certificate' hideResizeBtn={false} isMax={false}/>
+      <Windows  title={activeFolder} hideResizeBtn={false} isMax={false}/>
       <div className="content">
-          <NavPane />
+          <NavPane onDataSend={handleDataFromNavPane}/>
+          <div className="image-container">
+            { imageURLS?.map(({key,src,label}) => (
+              <div className="img-label" key={key}>
+                <img className="imgs"  src={src} alt={`img-${key}-${label}`} width={120} />
+                <p className="lbl">{label}</p>
+              </div>
+              ))}
+          </div>
       </div>
     </div>
     
   );
 }
+
+export default Cert;
--- src/components/UI/FileExplorer/NavPane.tsx
+++ src/components/UI/FileExplorer/NavPane.tsx
@@ -1,11 +1,24 @@
 import "./NavPane.css";
 
-const NavPane = () => {
+interface ChildProps {
+  onDataSend: (activeFolder: string) => void;
+}
+const NavPane:React.FC<ChildProps> = ({onDataSend}) => {
+  let folder="";
+  const handleClick = (key=3) => {
+    if(key == 3) folder="Certificate";
+    if(key == 1) folder="Recent";
+    if(key == 2) folder="Home";
+    if(key == 4) folder="Tech Stack";
+
+    onDataSend(folder);
+  }
   return (
     <div className="nav-pane">
-      <div className="nav-home nav-pane-btn">Home</div>
-      <div className="nav-cert nav-pane-btn">Certificate</div>
-      <div className="nav-tech-stack nav-pane-btn">Tech Stack</div>
+      <div className="nav-recent nav-pane-btn"     onClick={() => handleClick(1)}>Recent</div>
+      <div className="nav-home nav-pane-btn"       onClick={() => handleClick(2)}>Home</div>
+      <div className="nav-cert nav-pane-btn"       onClick={() => handleClick(3 )}>Certificate</div>
+      <div className="nav-tech-stack nav-pane-btn" onClick={() => handleClick(4)}>Tech Stack</div>
     </div>
   );
 }
