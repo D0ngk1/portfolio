a11203aefaf266ac87e7222c07317a0738a9107d
Daryl Guzman <darylgzmn@gmail.com>
2025-11-22 20:03:37 -1000

    style: Add some entries on Overview page

--- src/components/Layout/Experience/TabFeatures.tsx
+++ src/components/Layout/Experience/TabFeatures.tsx
@@ -1,5 +1,9 @@
 import {proj} from "@/components/Layout/Experience/TabProjects.tsx";  
 import {useRef,useState,useEffect} from 'react';
+const feat = [
+  {name:'Software Engineer',description:'1.5 years of Experience'},
+  ...proj,
+];
 const TabFeatures =()=> {
   const recentRef = useRef<HTMLDivElement>(null);
   const [activeIndex,setActiveIndex] = useState(0);
@@ -25,7 +29,7 @@ const TabFeatures =()=> {
       <div className="feat-container">
         <div className="recent-feat-container" ref={recentRef}>
 
-          {proj.map((name,i) => {
+          {feat.map((name,i) => {
             if (name.name !== 'Dictionary') return (
               <div  style={{ backgroundImage:"url("+name.image+")" }} className={`recent-feat feat-${name.name.replace(/\s+/g,'-').toLowerCase()}`}
                key={i}
@@ -37,9 +41,9 @@ const TabFeatures =()=> {
               const index = activeIndex;
               if(index > 0) setActiveIndex(index-1)
             }}></div> )}
-             {activeIndex!=2 &&(<div className="right-slide slide-btn" onClick={()=>{
+             {activeIndex!=3 &&(<div className="right-slide slide-btn" onClick={()=>{
               const index = activeIndex;
-              if(index < 2) setActiveIndex(index+1)
+              if(index < 3) setActiveIndex(index+1)
             }}></div>)}
         </div>
       </div>
