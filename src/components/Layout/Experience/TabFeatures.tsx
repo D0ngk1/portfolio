85f779eeb44c108c5976ec358392fa177aa953cc
Daryl Guzman <darylgzmn@gmail.com>
2025-11-23 20:56:00 -1000

    style: add feature section inside overview tab

--- src/components/Layout/Experience/TabFeatures.tsx
+++ src/components/Layout/Experience/TabFeatures.tsx
@@ -27,24 +27,41 @@ const TabFeatures = () => {
   return (
     <>
       <div className="feat-container">
-        <div className="recent-feat-container" ref={recentRef}>
-          {feat.map((name, i) => {
-            if (name.name !== 'Dictionary') return (
-              <div style={{ ...(name.image ? { backgroundImage: `url(${name.image})` } : {}) }} className={`recent-feat feat-${name.name.replace(/\s+/g, '-').toLowerCase()}`}
-                key={i}
-              >
-                <h1>{name.name}</h1>
-              </div>
-            )
-          })}
-          {activeIndex != 0 && (<div className="left-slide slide-btn" onClick={() => {
-            const index = activeIndex;
-            if (index > 0) setActiveIndex(index - 1)
-          }}></div>)}
-          {activeIndex != 3 && (<div className="right-slide slide-btn" onClick={() => {
-            const index = activeIndex;
-            if (index < 3) setActiveIndex(index + 1)
-          }}></div>)}
+        <div className="feat-intro"><h1>A tech enthusiast equipped with strong programming, cybersecurity and IT skills.</h1></div>
+        <div className="feat-proj-wrapper">
+          <h2 className="feat-label">Recent Projects</h2>
+          <div className="recent-feat-container" ref={recentRef}>
+            {feat.map((name, i) => {
+              if (name.name !== 'Dictionary') return (
+                <div style={{ ...(name.image ? { backgroundImage: `url(${name.image})` } : {}) }} className={`recent-feat feat-${name.name.replace(/\s+/g, '-').toLowerCase()}`}
+                  key={i}
+                >
+                  <h1>{name.name}</h1>
+                </div>
+              )
+            })}
+            {activeIndex != 0 && (<div className="left-slide slide-btn" onClick={() => {
+              const index = activeIndex;
+              if (index > 0) setActiveIndex(index - 1)
+            }}></div>)}
+            {activeIndex != 3 && (<div className="right-slide slide-btn" onClick={() => {
+              const index = activeIndex;
+              if (index < 3) setActiveIndex(index + 1)
+            }}></div>)}
+          </div>
+        </div>
+        <div className="feat-skill-wrapper">
+          <div className="feat-skill-content">
+            <div className="feat-skill-items">Networking</div>
+            <div className="feat-skill-items">Programming</div>
+            <div className="feat-skill-items">OOP</div>
+            <div className="feat-skill-items">Frontend</div>
+            <div className="feat-skill-items">Backend</div>
+            <div className="feat-skill-items">IT Security</div>
+            <div className="feat-skill-items">Unix/Linux</div>
+            <div className="feat-skill-items">Windows</div>
+            <div className="feat-skill-items">Troubleshooting</div>
+          </div>
         </div>
       </div>
     </>
