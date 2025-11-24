8cd5e7a0386c7f7772b852e24c186af018148aec
Bhadz <darylgzmn@gmail.com>
2025-11-23 16:57:30 -1000

    style:change wallpaper

--- src/components/Layout/Experience/TabFeatures.tsx
+++ src/components/Layout/Experience/TabFeatures.tsx
@@ -1,50 +1,50 @@
-import {proj} from "@/components/Layout/Experience/TabProjects.tsx";  
-import {useRef,useState,useEffect} from 'react';
+import { proj } from "@/components/Layout/Experience/TabProjects.tsx";
+import { useRef, useState, useEffect } from 'react';
 const feat = [
-  {name:'Software Engineer',description:'1.5 years of Experience'},
+  { name: 'Software Engineer', description: '1.5 years of Experience', image: '' },
   ...proj,
 ];
-const TabFeatures =()=> {
+const TabFeatures = () => {
   const recentRef = useRef<HTMLDivElement>(null);
-  const [activeIndex,setActiveIndex] = useState(0);
-  const centerItem = (index:number) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const centerItem = (index: number) => {
     const container = recentRef.current;
-    if(!container) return;
-      
+    if (!container) return;
+
     const item = container.children[index] as HTMLElement;
-    if(!item) return;
+    if (!item) return;
     const containerWidth = container.clientWidth;
     const itemWidth = item.clientWidth;
     const itemLeft = item.offsetLeft;
 
-    const scrollTo= itemLeft - containerWidth/2+itemWidth/2;
-    container.scrollTo({left:scrollTo,behavior:"smooth"});
+    const scrollTo = itemLeft - containerWidth / 2 + itemWidth / 2;
+    container.scrollTo({ left: scrollTo, behavior: "smooth" });
   };
-  useEffect(()=> {
+  useEffect(() => {
     centerItem(activeIndex);
 
-  },[activeIndex]);
+  }, [activeIndex]);
   return (
     <>
       <div className="feat-container">
         <div className="recent-feat-container" ref={recentRef}>
-
-          {feat.map((name,i) => {
+          {feat.map((name, i) => {
             if (name.name !== 'Dictionary') return (
-              <div  style={{ backgroundImage:"url("+name.image+")" }} className={`recent-feat feat-${name.name.replace(/\s+/g,'-').toLowerCase()}`}
-               key={i}
-               >
+              <div style={{ ...(name.image ? { backgroundImage: `url(${name.image})` } : {}) }} className={`recent-feat feat-${name.name.replace(/\s+/g, '-').toLowerCase()}`}
+                key={i}
+              >
                 <h1>{name.name}</h1>
               </div>
-            )})}
-            {activeIndex!=0 &&( <div className="left-slide slide-btn" onClick={()=>{
-              const index = activeIndex;
-              if(index > 0) setActiveIndex(index-1)
-            }}></div> )}
-             {activeIndex!=3 &&(<div className="right-slide slide-btn" onClick={()=>{
-              const index = activeIndex;
-              if(index < 3) setActiveIndex(index+1)
-            }}></div>)}
+            )
+          })}
+          {activeIndex != 0 && (<div className="left-slide slide-btn" onClick={() => {
+            const index = activeIndex;
+            if (index > 0) setActiveIndex(index - 1)
+          }}></div>)}
+          {activeIndex != 3 && (<div className="right-slide slide-btn" onClick={() => {
+            const index = activeIndex;
+            if (index < 3) setActiveIndex(index + 1)
+          }}></div>)}
         </div>
       </div>
     </>
