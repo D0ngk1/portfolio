db6c5ba8a92f0fabbad89c2eeaff152d95ad88ec
Daryl Guzman <darylgzmn@gmail.com>
2025-12-22 20:36:10 -1000

    refactor: implement grid display on experience tab

--- src/components/Layout/Experience/TabExperience.tsx
+++ src/components/Layout/Experience/TabExperience.tsx
@@ -7,42 +7,33 @@ const workExperience = [
 const TabProjects = () => {
   return (
     <>
-      <div className="exp-con-menu">
-      <span className="vertical-line-center"></span>
-        <div className="vertical-line left-box ">
-          <div className="item item-left">
-              <div className="move-to-right">
-               <span className="horizontal-line-left horizontal-line"></span> 
-                <span className="circle-left circle"></span>
-               <div className="item-left-content">
-                 <h2 className="company">Relatives & Friends</h2>
-                 <h4 className="job-title">Personal IT Specialist</h4>
-                 <h4 className="year">Current - 2014</h4>
-               </div>
-              </div>
-          </div>
-        </div>
-        <div className="vertical-line right-box">
-            <div className="item item-right item-right-first">
-            <span className="horizontal-line-right horizontal-line"></span>
-             <span className="circle-right circle"></span>
-               <div className="item-right-content">
-                 <h2 className="company">Relatives & Friends</h2>
-                 <h4 className="job-title">Personal IT Specialist</h4>
-                 <h4 className="year">Current - 2014</h4>
-               </div>
+      <div className="exp-con-menu">   
+        <div className="right-box">
+        <span className="vertical-line-center"></span>
+            <div className="item item-right-first">
+                <div className="indicator-icon indicator-icon-left">
+                  <span className="horizontal-line-left horizontal-line"></span>
+                  <span className="circle-left circle"></span>
+                </div>
+                <div className="item-left-content item-content">
+                     <h2 className="company">Relatives & Friends</h2>
+                     <h4 className="job-title">Personal IT Specialist</h4>
+                     <h4 className="year">Current - 2014</h4>
+                 </div>
             </div>
           { workExperience.map(({companyName,year,role}) => (
-              <div className="item item-right" key={`${companyName}${year}`}>
-              { companyName!='' && (<span className="horizontal-line-right horizontal-line"></span> )}
-               <span className="circle-right circle"></span>
-               <div className="item-right-content">
-                <h2 className="company">{companyName}</h2>
-                <h4 className="job-title">{role}</h4>
-                <h5 className="year">{year}</h5>
-               </div>
-              </div>
-            ))}
+            <div className="item item-right" key={`${companyName}${year}`}>
+                  <div className="indicator-icon">
+                    <span className="horizontal-line-right horizontal-line"></span>
+                    <span className="circle-right circle"></span>
+                  </div>
+                  <div className="item-right-content item-content">
+                    <h2 className="company">{companyName}</h2>
+                    <h4 className="job-title">{role}</h4>
+                    <h5 className="year">{year}</h5>
+                   </div>
+            </div>
+           ))}
         </div>                              
       </div>
     </>
